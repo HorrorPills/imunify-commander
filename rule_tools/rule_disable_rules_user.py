7c820bac9af0e94b85d5ca2aabd5f3c11ca79f1a
HorrorPills <rafalmio90@gmail.com>
2023-08-25 14:53:55 +0200

    update

--- rule_tools/rule_disable_rules_user.py
+++ rule_tools/rule_disable_rules_user.py
@@ -2,9 +2,25 @@ import os
 import config
 
 def append_rules_to_file(file_path, rule_ids):
-    for rule_id in rule_ids:
-        command = f"echo 'SecRuleRemoveById {rule_id}' | sudo tee -a {file_path} > /dev/null"
-        os.system(command)
+    with open(file_path, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        inside_block = False
+
+        for line in lines:
+            if line.strip() == '<IfModule mod_security2.c>':
+                inside_block = True
+                file.write(line)
+            elif line.strip() == '</IfModule>':
+                inside_block = False
+                file.write('\n')  # Add a newline before appending new rules
+                for rule_id in rule_ids:
+                    file.write(f'SecRuleRemoveById {rule_id}\n')
+                file.write(line)
+            elif inside_block:
+                file.write(line)
+            else:
+                file.write(line)
 
 def main():
     os.system('clear')
@@ -20,14 +36,20 @@ def main():
     print("+-------------------------------------------+")
     rule_ids = rule_ids_input.split()
 
-    file_path = f'/usr/local/apache/conf/userdata/{username}/{username}.conf'
+    folder_path = f'/usr/local/apache/conf/userdata/{username}'
+    file_path = f'{folder_path}/{username}.conf'
 
+    if not os.system(f"sudo test -d {folder_path}"):
+        os.system(f"sudo mkdir -p {folder_path}")
+        os.system(f"sudo chmod 755 {folder_path}")
+    
     if not os.path.exists(file_path):
         os.system(f"sudo sh -c 'echo \"<IfModule mod_security2.c>\" > {file_path}'")
         os.system(f"sudo sh -c 'echo \"</IfModule>\" >> {file_path}'")
 
     append_rules_to_file(file_path, rule_ids)
 
+    print("+-------------------------------------------+")
     print("Rebuild httpd configuration?")
     print(" ")
     print("1. Yes")
