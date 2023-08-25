e1a2889cc70a0b5e349c8c0942206c9deb19020b
HorrorPills <rafalmio90@gmail.com>
2023-08-25 11:27:55 +0200

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import config  # Import the VERSION variable from config.py
 
 def show_menu():
-    os.system('clear')
+    os.system('cls')
     print("")
     print("+-------------------------------------------+")
     print(f"IMUNIFY-COMMANDER {config.VERSION} | WEBD.pl")
