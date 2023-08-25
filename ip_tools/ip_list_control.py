70a8fb804f8ce826197942d6ba2a4542d064a2d7
HorrorPills <rafalmio90@gmail.com>
2023-08-25 11:15:13 +0200

    update

--- ip_tools/ip_list_control.py
+++ ip_tools/ip_list_control.py
@@ -15,6 +15,8 @@ def remove_ip_from_whitelist(ip_address):
     subprocess.run(f"sudo imunify360-agent whitelist ip delete {ip_address}", shell=True)
 
 def show_list_control_menu():
+    import ip_tools_menu  # Import locally to avoid circular import
+
     os.system('clear')
     ip_address = input("Enter IP address: ")
     while True:
@@ -32,36 +34,16 @@ def show_list_control_menu():
         elif choice == "2":
             action = "remove"
         elif choice == "3":
-            ip_tools_menu.show_menu()
+            ip_tools_menu.show_menu()  # Use the locally imported module
             break
         else:
             print("Invalid choice. Please choose a valid option.")
             continue
 
         if action == "add":
-            print("Adding IP to:")
-            print("1. Blacklist")
-            print("2. Whitelist")
-            print("+-------------------------------------------+")
-            action_choice = input("Choose number: ")
-            if action_choice == "1":
-                add_ip_to_blacklist(ip_address)
-            elif action_choice == "2":
-                add_ip_to_whitelist(ip_address)
-            else:
-                print("Invalid choice. Please choose a valid option.")
+            # ... (your existing code)
         elif action == "remove":
-            print("Removing IP from:")
-            print("1. Blacklist")
-            print("2. Whitelist")
-            print("+-------------------------------------------+")
-            action_choice = input("Choose number: ")
-            if action_choice == "1":
-                remove_ip_from_blacklist(ip_address)
-            elif action_choice == "2":
-                remove_ip_from_whitelist(ip_address)
-            else:
-                print("Invalid choice. Please choose a valid option.")
+            # ... (your existing code)
 
 if __name__ == "__main__":
     show_list_control_menu()
