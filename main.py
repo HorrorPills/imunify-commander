7f58eedc155b8f2e456854e4d8f6769e97e71d56
HorrorPills <rafalmio90@gmail.com>
2023-08-25 11:37:59 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # menu.py
 import os
-import config  # Import the VERSION variable from config.py
+import config
 
 def show_menu():
     os.system('cls')
@@ -19,10 +19,10 @@ def show_menu():
         import ip_tools.ip_tools_menu as ip_tools_menu
         ip_tools_menu.show_menu()
     elif choice == "2":
-        import rule_tools.rule_tools_menu as rule_tools_menu
+        import rule_tools.rule_tools.rule_tools_menu as rule_tools_menu
         rule_tools_menu.show_menu()
     elif choice == "3":
-        import scan_tools.scan_tools_menu as scan_tools_menu
+        import scan_tools.scan_tools.scan_tools_menu as scan_tools_menu
         scan_tools_menu.show_menu()
     else:
         print("Invalid choice. Please choose a valid option.")
