d4fa6576005cb1a1d3b9d80564d39879a216deee
HorrorPills <rafalmio90@gmail.com>
2023-08-30 10:51:24 +0200

    update

--- ip_tools/ip_status.py
+++ ip_tools/ip_status.py
@@ -27,11 +27,11 @@ def get_ip_expiration(ip_address):
         if len(fields) >= 5:
             expiration_timestamp = int(fields[2])
             expiration_date = datetime.fromtimestamp(expiration_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-            return expiration_date
+            return expiration_timestamp, expiration_date
         else:
-            return "Not found"
+            return None, None
     except subprocess.CalledProcessError:
-        return "Not found"
+        return None, None
 
 def run_check_ip_menu():
     while True:
@@ -44,8 +44,15 @@ def run_check_ip_menu():
         
         is_blacklisted = is_ip_in_blacklist(ip_address)
         is_whitelisted = is_ip_in_whitelist(ip_address)
-        expiration_date = get_ip_expiration(ip_address)
+        expiration_timestamp, expiration_date = get_ip_expiration(ip_address)
         
+        if expiration_timestamp is not None:
+            current_timestamp = int(time.time())
+            days_until_expiration = max(0, (expiration_timestamp - current_timestamp) // SECONDS_IN_A_DAY)
+            expiration_status = f"{days_until_expiration} days"
+        else:
+            expiration_status = "Not found"
+
         os.system('clear')
         print("")
         print("+-------------------------------------------+")
@@ -57,7 +64,9 @@ def run_check_ip_menu():
         print("+-------------------------------------------+")
         print(f"Blacklist: {is_blacklisted}")
         print(f"Whitelist: {is_whitelisted}")
-        print(f"Expiration: {expiration_date}")
+        print(f"Expiration: {expiration_status}")
+        if expiration_date:
+            print(f"Expiration Date: {expiration_date}")
         print("+-------------------------------------------+")
         print("1. Check Status of another IP Address")
         print("2. Back")
